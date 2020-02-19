677155c7f9abd9a675c80ad8f48f9d3f726b7414
Martin2207 <481700831@alumnos.utzac.edu.mx>
2020-02-19 16:40:08 -0600

    Commit necesario para enviar pull

--- src/routes/cities.js
+++ src/routes/cities.js
@@ -13,8 +13,17 @@ router.get('/',(req,res)=>{
         }
     })
 });
-
-//AGERGAR 
+router.get('/lenguageByCountry/:continent',(req,res)=>{
+    const {continent} = req.params;
+    mysqlConnection.query("select countrylanguage.*,country.Continent FROM countrylanguage JOIN country on countrylanguage.CountryCode = country.Code WHERE country.Continent = ?",[continent],(err,rows,fields)=>{
+        if(err){
+            console.log("Error: ",err);
+        }else{
+            res.json(rows);
+        }
+    })
+});
+//AGRERGAR 
 router.post('/',function (req,res){
     const {Name,CountryCode,District,Population} = req.body;
     mysqlConnection.query("INSERT INTO city VALUES(NULL,?,?,?,?)",[Name,CountryCode,District,Population],(err,rows,fields)=>{
@@ -28,12 +37,28 @@ if(err){
 
 //ELIMINAR 
 router.delete('/:id', function (req,res){
-
+const {id} = req.params;
+mysqlConnection.query("DELETE FROM city WHERE id = ?",[id],(err,rows,field)=>{
+    if(err){
+        console.log(err);
+    }else{
+        res.json({"status":"city deleted"});
+    }
+        })
 });
 
 //ACTUALIZAR 
-router.put('/:id', function(rep,res){
-    
+router.put('/:id', function (req,res){
+    const {id} = req.params;
+    const {Name,CountryCode,District,Population} = req.body;
+
+    mysqlConnection.query("UPDATE city SET  Name =?,CountryCode=?,District=?,Population=? WHERE id =?",[Name,CountryCode,District,Population,id],(err,rows,field)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.json({"status":"city update"});
+        }
+            })
 });
 
 module.exports=router;
